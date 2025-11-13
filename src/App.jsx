cdb163ffdc6760d811b17ef7e8f628b5a60144a4
WissemJderi <asqr43641@gmail.com>
2025-11-13 15:47:44 +0100

    add radix ui and create components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
 function App() {
+  console.log("Print");
   return (
     <>
       <h1 className="text-2xl">PPS Links</h1>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
+      <App />
+    </Theme>
   </StrictMode>,
-)
+);
